35039778a72a19b9e196da78a85a8d4d41bb3c22
adamz <adam@zethrae.us>
2026-08-06 20:39:24 -0700

    0.7.2

--- counter/Package.swift
+++ counter/Package.swift
@@ -78,8 +78,8 @@ let package = Package(
   platforms: platforms,
   products: products,
   dependencies: [
-    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.7.1"),
-    .package(url: "https://github.com/SwiftTUI/swift-tui-swiftui.git", exact: "0.7.1"),
+    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.7.2"),
+    .package(url: "https://github.com/SwiftTUI/swift-tui-swiftui.git", exact: "0.7.2"),
   ],
   targets: targets,
   swiftLanguageModes: [.v6]
