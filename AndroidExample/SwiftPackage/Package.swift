0ca45502fed4efac40c8108ab86ce1161f0593e2
adamz <adam@zethrae.us>
2026-09-03 13:18:36 -0400

    0.10.1

--- AndroidExample/SwiftPackage/Package.swift
+++ AndroidExample/SwiftPackage/Package.swift
@@ -26,7 +26,7 @@ let package = Package(
   ],
   dependencies: [
     .package(path: "../../counter"),
-    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.10.0"),
+    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.10.1"),
   ],
   targets: [
     .target(
