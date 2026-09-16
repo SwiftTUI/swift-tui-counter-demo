0f25256f193e8e2188f03c2b7aeecaff9e82e9c7
adamz <adam@zethrae.us>
2026-09-16 04:58:45 +0100

    Release 0.13.5 multi-host counter

--- AndroidExample/SwiftPackage/Package.swift
+++ AndroidExample/SwiftPackage/Package.swift
@@ -26,7 +26,7 @@ let package = Package(
   ],
   dependencies: [
     .package(path: "../../counter"),
-    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.13.4"),
+    .package(url: "https://github.com/SwiftTUI/swift-tui.git", exact: "0.13.5"),
   ],
   targets: [
     .target(
